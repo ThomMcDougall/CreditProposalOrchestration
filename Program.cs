d20c83d7566ec9ef47468ceeb75131a2be82441c
Thomas McDougall <thomas.mcdougall@senff.com.br>
2025-04-16 17:45:42 -0300

    Program file content change

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿using ProposalOrchestrator.Workflows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProposalOrchestrator.WorkflowEngine;
 
-var workflow = CreditProposalWorkflow.Create();
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddTransient<Workflow>();
+    })
+    .Build();
+
+using var scope = host.Services.CreateScope();
+var workflow = scope.ServiceProvider.GetRequiredService<Workflow>();
 await workflow.RunAsync();
